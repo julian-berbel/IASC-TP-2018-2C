cdbbc061274bc51274547d86e16b2bacefdd0b46
Julián Berbel Alt <julianberbel@hotmail.com>
2018-12-01 14:55:20 -0300

    init project

---
+++ lib/tp_iasc.ex
@@ -0,0 +1,18 @@
+defmodule TpIasc do
+  @moduledoc """
+  Documentation for TpIasc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TpIasc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tp_iasc_test.exs
@@ -0,0 +1,8 @@
+defmodule TpIascTest do
+  use ExUnit.Case
+  doctest TpIasc
+
+  test "greets the world" do
+    assert TpIasc.hello() == :world
+  end
+end
