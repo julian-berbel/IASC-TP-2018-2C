c59163033e6fa4ab08e8b30eeeae7117567f6d5c
JessSaavedra <jessicasaavedraval@gmail.com>
2018-12-25 19:42:25 -0300

    Fragmentation is distributed among several nodes

--- lib/tp_iasc.ex
+++ lib/tp_iasc.ex
@@ -4,17 +4,22 @@ defmodule TpIasc do
 
   def start(_type, _args) do
     
-    Amnesia.Schema.create([node()])
-    Amnesia.Fragment.activate(Message, [node()])
+    nodes = [ node() | Node.list() ]
+    Amnesia.Schema.create(nodes)
+    Amnesia.Fragment.activate(Message, nodes)
     Amnesia.start()
     
-    DB.MessageDB.create(memory: [node()])
+    DB.MessageDB.create(memory: nodes)
     DB.MessageDB.wait()
 
     Queue.Supervisor.start_link
 
+    # Esto lo comento temporalmente porque al querer levantar dos nodos
+    # en simultáneo levanta este server en el mismo puerto y rompe
+    # Habría que mandarle un parámetro al start para saber si es el proceso
+    # principal y solo ejecutar esto en ese caso
     children = [
-      TpIasc.Server
+      # TpIasc.Server
     ]
 
     opts = [strategy: :one_for_one, name: TpIasc.Supervisor]
