38c8cc9f342cf3fea8d0997310b5944b90eb9f91
Julián Berbel Alt <julianberbel@hotmail.com>
2019-02-19 00:04:10 -0300

    get ip from ifconfig

--- bin/main.sh
+++ bin/main.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-iex --name "$1@127.0.0.1" --cookie aReallyStrongCookie -S mix
+
+IP=$(echo "$(ifconfig eth1 || ifconfig eth0 || ifconfig wlp2s0)" | grep -o -E "inet addr:[^ ]*" | cut -d ':' -f 2)
+
+elixir --name "main@$IP" --cookie aReallyStrongCookie -S mix run --no-halt
