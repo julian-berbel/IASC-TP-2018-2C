84e33a9ea44fc9be51d5a180490be74e735790b5
Julián Berbel Alt <julianberbel@hotmail.com>
2019-02-19 00:22:08 -0300

    make build.sh executable and print ip on startup

--- bin/main.sh
+++ bin/main.sh
@@ -2,4 +2,6 @@
 
 IP=$(echo "$(ifconfig eth1 || ifconfig eth0 || ifconfig wlp2s0)" | grep -o -E "inet addr:[^ ]*" | cut -d ':' -f 2)
 
+echo "Setting elixir node up in main@$IP"
+
 elixir --name "main@$IP" --cookie aReallyStrongCookie -S mix run --no-halt
