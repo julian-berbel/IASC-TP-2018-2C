137e908eefb88db4080679f678183fb1b88e3c52
Julián Berbel Alt <julianberbel@hotmail.com>
2019-02-20 23:05:19 -0300

    allow passing node to connect to to main.sh

--- bin/main.sh
+++ bin/main.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+if [ $# -eq 1 ]; then
+  echo $1 > cluster_nodes
+fi
+
 IP=$(echo "$(ifconfig eth1 || ifconfig eth0 || ifconfig wlp2s0)" | grep -o -E "inet addr:[^ ]*" | cut -d ':' -f 2)
 
 echo "Setting elixir node up in main@$IP"
 
-elixir --name "main@$IP" --cookie aReallyStrongCookie -S mix run --no-halt
+iex --name "main@$IP" --cookie aReallyStrongCookie -S mix run
